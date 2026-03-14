bdc9dccb7629a3fa872ee35dde44d7717ab0e086
kathiravan <kathiravan@kraniumtechnology.com>
2026-03-14 12:57:37 +0530

    initial commit

--- obj/Release/net10.0/TaskBoard.API.AssemblyInfo.cs
+++ obj/Release/net10.0/TaskBoard.API.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskBoard.API")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b46672693e2486299daa6ab998c8e9a11459b51")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskBoard.API")]
